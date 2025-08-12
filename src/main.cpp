17d81e47e8f27e9cdd7a068e3878d237190d2202
Philip Davis <philip.davis.me@gmail.com>
2025-08-12 17:54:44 -0500

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+#include "raylib.h"
+
+void test_window() {
+    InitWindow(1280, 720, "Test Window");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    test_window();
     return 0;
 }
